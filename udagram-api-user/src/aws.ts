649943c268d209b1daab0b5133d39be7c35bbf9e
James Abraham <abraham.james@vendease.com>
2022-06-17 13:58:01 +0100

    Update aws.js

--- udagram-api-user/src/aws.ts
+++ udagram-api-user/src/aws.ts
@@ -6,14 +6,6 @@ import {config} from './config/config';
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
 
-AWS.config.getCredentials((err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`AWS Credentials: ${AWS.config.credentials.accessKeyId}`);
-})
-
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: config.aws_region,
