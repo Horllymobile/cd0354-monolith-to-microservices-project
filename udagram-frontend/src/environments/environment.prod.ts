58e6c3357cadffbdebe9fe635456610678975721
James Abraham <abraham.james@vendease.com>
2022-06-17 16:13:35 +0100

    Updates

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a86413b971fa84ac189d2cdaae344733-112039126.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
