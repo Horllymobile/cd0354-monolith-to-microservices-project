11dad4cfbce359b33165285f4b7c556ba08ea462
James Abraham <abraham.james@vendease.com>
2022-06-18 15:14:01 +0100

    Update frontend docker

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -5,7 +5,6 @@ import {sequelize} from './sequelize';
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
 import { V0_FEED_MODELS } from './controllers/v0/model.index';
 
 
